fdbb63c3ec5ab9565b07774bcb720f10c35de1b0
Abe Artus <abeartus@yahoo.com>
2025-04-01 00:23:41 -0500

    Fixing the embed classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,23 @@ export default function Home() {
       <img className="cat" src="/u.png" alt="ube logo" />
       ube-music
       <iframe 
+        className="sc-player"
         width="80%"
-        height="166" allow="autoplay"
+        height="166"
+        allow="autoplay"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1872600960&color=%23ff0000&auto_play=true&hide_related=true&show_comments=true&show_user=false&show_reposts=false&show_teaser=false">
       </iframe>
-      <iframe className="current-song" width="80%" height="166" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1599846087%3Fsecret_token%3Ds-PsK7UgDVYV9&color=%239c9c94&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"></iframe>
       <iframe 
-        className="current-song"
+        className="sc-player"
+        width="80%"
+        height="166"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1599846087%3Fsecret_token%3Ds-PsK7UgDVYV9&color=%239c9c94&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"
+      >
+      </iframe>
+      <iframe 
+        className="sc-player"
         width="80%" 
         height="166" 
-        allow="autoplay" 
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1890825261&color=%239c9c94&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=true&show_teaser=false"></iframe><div 
         style={{
           "fontSize": "10px",
